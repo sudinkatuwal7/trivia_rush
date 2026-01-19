ea8a7086d05cbab8d0ed61aff3712d7981949d9c
Sudin Katuwal <sudcrack2024@gmail.com>
2026-01-19 02:25:59 -0600

    Implement Question class with text and answer attributes

---
+++ question_model.py
@@ -0,0 +1,17 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
+
+
+
+
+#---------------------------------------------------------
+# new_q = Question("a for apple b, for tero bau", "False")
+#
+# print(new_q.text)
+# print(new_q.answer)
+
